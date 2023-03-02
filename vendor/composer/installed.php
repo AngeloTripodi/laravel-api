ffe0e89cbfeb9016eb56404492f9f8e2ccea0fbb
AngeloTripodi <tripodi.angelo@gmail.com>
2023-03-02 17:25:44 +0100

    setup project

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0b7e0e6d27adc771c384792b6fb01296c3325538',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -383,9 +383,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0b7e0e6d27adc771c384792b6fb01296c3325538',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
